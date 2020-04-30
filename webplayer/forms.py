300f46bc624fb8f5bb7120731603b66c0db859af
İsmail Demirbilek <ce.demirbilek@gmail.com>
2020-04-30 17:11:20 +0300

    remove name field

--- webplayer/forms.py
+++ webplayer/forms.py
@@ -3,5 +3,5 @@ from django.forms import PasswordInput
 
 
 class PlayerForm(forms.Form):
-    name = forms.CharField(label='İsminiz', max_length=100)
+    # name = forms.CharField(label='İsminiz', max_length=100)
     password = forms.CharField(label='Şifre', max_length=100, widget=PasswordInput, required=False)
