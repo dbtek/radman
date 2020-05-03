82e325dea945f6c6fbdd371c0ef847e0b28a77a0
İsmail Demirbilek <ce.demirbilek@gmail.com>
2020-05-03 17:08:38 +0300

    add slug based slim player

--- webplayer/urls.py
+++ webplayer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from webplayer.views import player
+from webplayer.views import player, player_slim
 
 urlpatterns = [
     path('play/<uuid:uuid>/', player),
+    path('p/<slug:slug>/', player_slim),
 ]
--- webplayer/views.py
+++ webplayer/views.py
@@ -5,22 +5,14 @@ from stations.models import Mount
 from stations.password import verify_password, hash_password
 from webplayer.forms import PlayerForm
 
-
-def player(request, uuid):
+def player_slim(request, slug):
     try:
-        m = Mount.objects.get(pk=uuid)
+        m = Mount.objects.get(slug=slug)
     except Mount.DoesNotExist:
         raise Http404("Kanal bulunamadı")
 
-    def render_player():
-        return render(request, 'player.html', {
-            'stationName': m.station.name,
-            'mountName': m.name,
-            'streamUrl': m.get_stream_url()
-        })
-
     if m.password is None:
-        return render_player()
+        return render_player(request, m)
     else:
         if request.POST:
             password = request.POST['password']
@@ -28,10 +20,10 @@ def player(request, uuid):
             if not verify_password(m.password, password):
                 form = PlayerForm(request.POST)
                 form.add_error('password', 'Şifre doğru değil.')
-                return render(request, 'player_form.html', {'mountId': m.id, 'form': form})
+                return render(request, 'player_form.html', {'action': '/p/%s/' % m.slug, 'form': form})
             else:
                 request.session['password'] = password
-                return render_player()
+                return render_player(request, m)
         else:
             if 'mount_token' in request.session:
                 # get mount token from session
@@ -40,9 +32,51 @@ def player(request, uuid):
                 if not hash_password(m.password) == mount_token:
                     form = PlayerForm(None)
                     form.add_error('password', 'Şifre doğru değil.')
-                    return render(request, 'player_form.html', {'mountId': m.id, 'form': form})
+                    return render(request, 'player_form.html', {'action': '/p/%s/' % m.slug, 'form': form})
                 else:
-                    return render_player()
+                    return render_player(request, m)
 
             form = PlayerForm(None)
-            return render(request, 'player_form.html', {'mountId': m.id, 'form': form})
+            return render(request, 'player_form.html', {'action': '/p/%s/' % m.slug, 'form': form})
+
+def player(request, uuid):
+    try:
+        m = Mount.objects.get(pk=uuid)
+    except Mount.DoesNotExist:
+        raise Http404("Kanal bulunamadı")
+
+    if m.password is None:
+        return render_player(request, m)
+    else:
+        if request.POST:
+            password = request.POST['password']
+            # do password check
+            if not verify_password(m.password, password):
+                form = PlayerForm(request.POST)
+                form.add_error('password', 'Şifre doğru değil.')
+                return render(request, 'player_form.html', {'action': '/play/%s/' % m.id, 'form': form})
+            else:
+                request.session['password'] = password
+                return render_player(request, m)
+        else:
+            if 'mount_token' in request.session:
+                # get mount token from session
+                mount_token = request.session['mount_token']
+                # do password check
+                if not hash_password(m.password) == mount_token:
+                    form = PlayerForm(None)
+                    form.add_error('password', 'Şifre doğru değil.')
+                    return render(request, 'player_form.html', {'action': '/play/%s/' % m.id, 'form': form})
+                else:
+                    return render_player(request, m)
+
+            form = PlayerForm(None)
+            return render(request, 'player_form.html', {'action': '/play/%s/' % m.id, 'form': form})
+
+
+def render_player(request, m):
+    return render(request, 'player.html', {
+        'stationName': m.station.name,
+        'mountName': m.name,
+        'streamUrl': m.get_stream_url()
+    })
