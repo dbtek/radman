c82aa90f4b2af67c7a4df8b176dc6ad3e082063d
İsmail Demirbilek <ce.demirbilek@gmail.com>
2020-11-16 13:51:13 +0300

    update player in admin

--- stations/admin.py
+++ stations/admin.py
@@ -18,7 +18,6 @@ class StationAdmin(admin.ModelAdmin):
 
 @admin.register(Mount)
 class MountAdmin(admin.ModelAdmin):
-    readonly_fields = ('mount_point', 'mount_id')
 
     def get_queryset(self, request):
         if request.user.is_superuser:
@@ -47,14 +46,20 @@ class PlayerAdmin(admin.ModelAdmin):
     actions = [make_active, make_inactive]
 
     def play_widget(self, obj):
-        return mark_safe('<audio controls src="%s/%s">%s</audio>' % (
-            obj.mount.station.base_url,
-            obj.mount.mount_point,
-            '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">'
+        id = str(obj.id);
+        url = obj.get_stream_url()
+        return mark_safe(
+            '<div>'
+            '<audio type="audio/mpeg" id="audio-' + id + '">'
+            '<source src="' + url + '" type="audio/mpeg" />'
+            '</audio>'
+            '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"'
+                                                     'onclick="document.querySelector(\'#audio-' + id + '\').play()">'
             '<path d="M0 0h24v24H0z" fill="none"/>'
             '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>'
             '</svg>'
-        ))
+            '</div>'
+        )
 
     play_widget.short_description = _('Play')
 
