7f2bddf629937626bbb7faea61db463a544227b1
İsmail Demirbilek <ce.demirbilek@gmail.com>
2021-01-16 21:49:19 +0300

    file format

--- stations/admin.py
+++ stations/admin.py
@@ -57,14 +57,14 @@ class PlayerAdmin(admin.ModelAdmin):
         return mark_safe(
             '<div>'
             '<audio type="audio/mpeg" id="audio-' + id + '">'
-                                                         '<source src="' + url + '" type="audio/mpeg" />'
-                                                                                 '</audio>'
-                                                                                 '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"'
-                                                                                 'onclick="document.querySelector(\'#audio-' + id + '\').play()">'
-                                                                                                                                    '<path d="M0 0h24v24H0z" fill="none"/>'
-                                                                                                                                    '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>'
-                                                                                                                                    '</svg>'
-                                                                                                                                    '</div>'
+            '<source src="' + url + '" type="audio/mpeg" />'
+            '</audio>'
+            '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"'
+            'onclick="document.querySelector(\'#audio-' + id + '\').play()">'
+            '<path d="M0 0h24v24H0z" fill="none"/>'
+            '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>'
+            '</svg>'
+            '</div>'
         )
 
     play_widget.short_description = _('Play')
