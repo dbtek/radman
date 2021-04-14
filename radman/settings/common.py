59e3c48d9f854809c464f03e19f217ccf0f7b667
Ismail Demirbilek <ce.demirbilek@gmail.com>
2021-04-14 23:41:08 +0300

    fix using staticfiles_dirs with static_root

--- radman/settings/common.py
+++ radman/settings/common.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,6 +131,9 @@ LOCALE_PATHS = (
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'static'),
-]
+if sys.argv[1] == 'runserver':
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, '..', 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
